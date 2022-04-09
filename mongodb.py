e83f3f8b85b2dd8a256e342c2391cbb95c25fb78
Andre Lesnick <andrecl@bu.edu>
2022-04-09 15:24:59 -0400

    added some functions

--- mongodb.py
+++ mongodb.py
@@ -1,4 +1,5 @@
 import pymongo
+import json
 
 class Mongo:
     def __init__(self):
@@ -6,29 +7,53 @@ class Mongo:
         self.db = self.client.twitter_trader
         self.portfolio = self.db.portfolio
         self.profits = self.db.profits
-        print(self.db.command("serverStatus"))
+        # print(self.db.command("serverStatus"))
 
-# returns portfolio from DB as dict
-def getPortfolio():
-    pass
-# takes in dict of new portfolio and returns JSON
-def JSONifyPortfolio(portfolio_dict):
-    pass
-# takes in dict of new portfolio and returns tuple of what to buy and sell
-def getTrades(portfolio_dict):
-    pass
-# takes in JSON of new portfolio and updates DB
-def updatePortfolio(portfolio_json):
-    pass
-# takes in current profit and adds to DB
-def updateProfits(net_profit):
-    pass
+    # returns portfolio from DB as dict
+    def getPortfolio(self):
+        cursor = self.portfolio.find()
+        lst = list(cursor)
+        dict = {}
+        for entry in lst:
+            dict[entry['company']] = entry['price']
+        return dict
+        
+    # takes in dict of new portfolio and returns JSON
+    def JSONifyPortfolio(self,portfolio_dict):
+        json.dumps(portfolio_dict)
+    # takes in dict of new portfolio and returns tuple of what to buy and sell -- (to_buy, to_sell) tuple
+    def getTrades(self,new_portfolio):
+        old_portfolio = self.getPortfolio()
+        to_buy = []
+        to_sell = []
+        for co in old_portfolio:
+            if not co in new_portfolio:
+                to_sell.append(co)
+        for co in new_portfolio:
+            if not co in old_portfolio:
+                to_buy.append(co)
+        return (to_buy,to_sell)
+    # takes in dict of new portfolio and updates DB
+    def updatePortfolio(self,portfolio_dict):
+        self.portfolio.delete_many({})
+        lst = []
+        for co in portfolio_dict:
+            doc = {"company": co, "price": portfolio_dict[co]}
+            lst.append(doc)
+        self.portfolio.insert_many(lst)
+
+    # takes in current profit and adds to DB
+    def updateProfits(self,net_profit):
+        pass
 
 
 
 if __name__ == "__main__":
     mongo = Mongo()
-    print(mongo.portfolio.insert_one({
-        "company": "$APL",
-        "price": "1002"
-        }))
+    port = mongo.getPortfolio()
+    example_dict = {"apple":100,"pear":200,"potato":50,"pineapple":99}
+    print("Current port:\n", port)
+    mongo.updatePortfolio(example_dict) 
+    print("\n\nNew port:", mongo.getPortfolio())
+
+    
