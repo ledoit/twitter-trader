06313cec4bf3f3881d1810e276e626ade433a062
Phil Ledoit <pledoit@bu.edu>
2022-04-09 17:31:54 -0400

    Update prices.py
    Deleted compare method as it's already in twitter.py

--- prices.py
+++ prices.py
@@ -1,19 +1,6 @@
 import yfinance
 
 
-def compare(old_50, new_50):
-    hold = sell = buy = []
-    for ticker in old_50:
-        if ticker in new_50:
-            hold += ticker
-        else:
-            sell += ticker
-    for ticker in new_50:
-        if ticker not in hold:
-            buy += ticker
-    return [hold, sell, buy]
-
-
 def get_price(ticker):
     company_info = yfinance.Ticker(ticker)
     # print(ticker, "last close: ", company_info.info['previousClose'])
