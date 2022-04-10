116a4bba18259fd4c4a1d2cdf9b75a815c2558e2
Phil Ledoit <pledoit@bu.edu>
2022-04-10 09:27:53 -0400

    Update mongodb.py
    Finished all functionality of processBalance and added timeloop in main

--- mongodb.py
+++ mongodb.py
@@ -1,11 +1,14 @@
 import pymongo
 import json
-from twitter import get_new_50
+from datetime import datetime as dt, timedelta as td
+from twitter import get_new_50, compare
 from prices import get_price
+
 
 class Mongo:
     def __init__(self):
-        self.client = pymongo.MongoClient("mongodb+srv://user:user@cluster0.gn2iv.mongodb.net/twitter_trader?retryWrites=true&w=majority")
+        self.client = pymongo.MongoClient(
+            "mongodb+srv://user:user@cluster0.gn2iv.mongodb.net/twitter_trader?retryWrites=true&w=majority")
         self.db = self.client.twitter_trader
         self.portfolio = self.db.portfolio
         self.balance = self.db.balance
@@ -15,69 +18,89 @@ class Mongo:
     def getPortfolio(self):
         cursor = self.portfolio.find()
         return list(cursor)
-        
-    # takes in dict of new portfolio and returns JSON
-    def JSONifyPortfolio(self,portfolio_dict):
-        json.dumps(portfolio_dict)
-    # takes in dict of new portfolio and returns tuple of what to buy and sell -- (to_buy, to_sell) tuple
-    def getTrades(self,new_portfolio):
-        old_portfolio = self.getPortfolio()
-        to_buy = []
-        to_sell = []
-        for co in old_portfolio:
-            if not co in new_portfolio:
-                to_sell.append(co)
-        for co in new_portfolio:
-            if not co in old_portfolio:
-                to_buy.append(co)
-        return (to_buy,to_sell)
-    # takes in dict of new portfolio and updates DB
-    def setPortfolio(self,new_portfolio):
-        self.portfolio.delete_many({})
-        lst = []
-        for co in new_portfolio:
-            doc = {"company": co, "price": new_portfolio[co]}
-            lst.append(doc)
-        self.portfolio.insert_many(lst)
 
-    # sets balance in DB as float
-    def setNewBalance(self,new_balance):  # not sponsored by NB
-        last_entry = self.getLatestBalance().next()
-        last_time = last_entry['time']
-        last_balance = last_entry['balance']
-        self.balance.insert_one({'time': last_time + 1, 'balance': new_balance})
-        return last_balance + new_balance
+    # takes in lst of new portfolio and updates DB
+    def setPortfolio(self, new_portfolio):
+        # storing a lst:
+        self.portfolio.delete_many([])
+        self.portfolio.insert_one(new_portfolio)
+
+        # # storing a dict:
+        # self.portfolio.delete_many({})
+        # lst = []
+        # for co in new_portfolio:
+        #     doc = {"company": co, "price": new_portfolio[co]}
+        #     lst.append(doc)
+        # self.portfolio.insert_many(lst)
+
+    # calculates total value of portfolio as float
+    def evalPortfolio(self, lst):
+        total = 0.0
+        for ticker in lst:
+            total += get_price(ticker)
+        return total
 
     # gets latest balance from DB as float
     def getLatestBalance(self):
-        return self.balance.find().limit(1).sort([('$natural',-1)])
+        return self.balance.find().limit(1).sort([('$natural', -1)])
 
+    # sets new balance in DB as float
+    def setNewBalance(self, new_stock, new_cash):
+        last_entry = self.getLatestBalance().next()
+        last_time = last_entry['time']
+        self.balance.insert_one({'time': last_time + 1, 'balance': new_stock, 'cash': new_cash})
 
-    def initDatabases(self,company_lst):
-        self.getPortfolio(company_lst)
+    # # takes in dict of new portfolio and returns JSON
+    # def JSONifyPortfolio(self,portfolio_dict):
+    #     json.dumps(portfolio_dict)
+    #
+    # # takes in dict of new portfolio and returns tuple of what to buy and sell -- (to_buy, to_sell) tuple
+    # def getTrades(self,new_portfolio):
+    #     old_portfolio = self.getPortfolio()
+    #     to_buy = []
+    #     to_sell = []
+    #     for co in old_portfolio:
+    #         if not co in new_portfolio:
+    #             to_sell.append(co)
+    #     for co in new_portfolio:
+    #         if not co in old_portfolio:
+    #             to_buy.append(co)
+    #     return (to_buy,to_sell)
+
+    def initDatabases(self, lst):
+        self.setPortfolio(lst)
         self.balance.delete_many({})
-        self.balance.insert_one({'time': 0, 'balance': 0})
+        self.balance.insert_one({'time': 0, 'stock': 0, 'cash': 0})
 
-    
     def processBalance(self):
-        old_portfolio = self.getPortofolio()
+        old_50 = self.getPortfolio()
         new_50 = get_new_50()
-        old_total = 0.0
-        new_total = 0.0
-        for ticker in old_portfolio:
-            old_total += get_price(ticker)
-        for ticker in new_50:
-            new_total += get_price(ticker)
-        latest_balance = self.getLatestBalance() - old_total + new_total
-        self.setNewBalance(latest_balance)
+        [hold, sell, buy] = compare(old_50, new_50)
+        cash_in = self.evalPortfolio(sell)
+        cash_out = self.evalPortfolio(buy)
+        latest_cash = self.getLatestBalance()['cash'] + cash_in - cash_out
+        latest_stock = self.evalPortfolio(hold) + cash_out
+        self.setNewBalance(latest_stock, latest_cash)
+        self.setPortfolio(hold + buy)
 
 
 if __name__ == "__main__":
+    # setting up mongo DB
     mongo = Mongo()
     port = mongo.getPortfolio()
-    example_lst = ["apple","pear","potato","pineapple"]
     print("Current port:\n", port)
-    new_50 = get_new_50()
-    mongo.setPortfolio(new_50)
     print("\n\nNew port:", mongo.getPortfolio())
-    
+
+    # initializing DB
+    mongo.initDatabases([])
+
+    # first call to processBalance
+    mongo.processBalance()
+    start_time = dt.now()
+
+    # infinite loop for fetching positivity ratings & prices, and updating the DB
+    while True:
+        # time buffer that lasts 1 day
+        if dt.now() - start_time >= td(days = 1):
+            start_time = dt.now()
+        mongo.processBalance()
